a31f7acff02aa0ac86d7ff165f977676cb5efd77
Hanncyber <hana.tamer1979@gmail.com>
2025-12-13 20:09:08 +0200

    nn

--- MainWindow.h
+++ MainWindow.h
@@ -31,8 +31,6 @@ private:
     QPushButton *patternButton;
     QPushButton *predictionButton;
     QPushButton *exitButton;
-    template <typename T>
-    void openChildWindow();
 };
 
 #endif // MAINWINDOW_H
